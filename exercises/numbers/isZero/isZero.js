2968e4e49d1f313de7cc9ca6b663f1f7441861ea
Savanna Vest <savest@davidson.edu>
2020-06-16 15:56:16 -0400

    first pass at numbers exercises

--- exercises/numbers/isZero/isZero.js
+++ exercises/numbers/isZero/isZero.js
@@ -4,17 +4,24 @@
  * @param {number} num - The input number
  * @returns {boolean} True if num is 0 and false otherwise
  */
+
+ num = []
+
 function isZero(num) {
   return num === 0;
 }
 
-if (require.main === module) {
+if (isZero(num) === 0) {
   console.log('Running sanity checks for isZero:');
-
-  console.log(isZero(0) === true);
-  console.log(isZero(1) === false);
-  console.log(isZero(-1) === false);
-  console.log(isZero(-100.201) === false);
+  console.log(isZero(num) === true);
+  return true; // not sure why this 'return' is appearing purple but others appear blue.
+  
+ // console.log(isZero(1) === false);
+ // console.log(isZero(-1) === false);
+ // console.log(isZero(-100.201) === false);
+} else {
+  console.log(isZero(num) === false);
+  return false; // not sure why this 'return' is appearing purple but others appear blue.
 }
 
 module.exports = isZero;
